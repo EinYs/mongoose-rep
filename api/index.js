bb702cb75e7d0db048ec9478245e88ef856b95ae
EinYs <ysunny32@gmail.com>
2019-01-22 20:38:24 +0900

    init

---
+++ api/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const { Nuxt, Builder } = require('nuxt')
+const express = require('express')
+
+var port = process.env.PORT || '3000'
+var isProd = process.env.NODE_ENV === 'production'
+
+//[Start Mongodb]
+const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);// fix for -> collection.ensureIndex is deprecated. Use createIndexes instead.
+mongoose.set('useMongoClient', true);// 
+mongoose.set('useFindAndModify', true);// 
+var uri = process.env.DBURI; // 테스트 heroku 에서는 test DB URI !
+mongoose.connect(uri, { useNewUrlParser: true},  function (err, res) {
+    console.log(err || 'MongoDB connected');
+});
+mongoose.Promise = require('bluebird');
+
+const app = express()
+
+
+//NUXT CONFIG
+let nuxtConfig = require('../nuxt.config.js')
+const nuxt = new Nuxt(nuxtConfig)
+//const nuxt = new Nuxt({ dev: !isProd })
+// 프로덕션 환경에서 빌드되지 않음.
+if (!isProd) {
+    const builder = new Builder(nuxt)
+    builder.build()
+}
+app.use(nuxt.render)
+
+app.listen(port, function () {
+    console.log('Server is listening on http://localhost:' + port);
+})
